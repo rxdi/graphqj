5c2f04340d0310a68941f5e5396c76ae3617603b
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2019-08-14 02:46:06 +0300

    feature(HotReload): hot reaload is up and workinggaga Cache for modules is left!

--- src/helpers/react-to-changes.ts
+++ src/helpers/react-to-changes.ts
@@ -5,6 +5,7 @@ import { loadFile } from './load-file';
 import { Config } from '../app/app.tokens';
 import { MakeAdvancedSchema } from './advanced-schema';
 import { deep } from './traverse/test';
+import { lazyTypes } from './lazy-types';
 
 function findMetaKey(path: string, meta: { [key: string]: string }) {
   return Object.keys(meta).find(k => meta[k] === path);
@@ -32,6 +33,12 @@ export async function reactToChanges(path: string, config: Config) {
       return false;
     });
   }
+  lazyTypes.clear();
+  Container.get(BootstrapService).Fields = {
+    mutation: {},
+    query: {},
+    subscription: {}
+  };
   await MakeAdvancedSchema(config);
   Container.get(ApolloService).init();
   // await SchemaIntrospection()
