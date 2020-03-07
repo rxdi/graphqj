8eccfc99de055e143ba078457c4f785062a8e508
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2020-03-07 15:12:20 +0200

    chore(package.json): version bump of dependencies @gapi core and voyager

--- examples/app/graph/resolvers/findUser.ts
+++ examples/app/graph/resolvers/findUser.ts
@@ -5,11 +5,5 @@ export async function foobar(root, payload, context, info) {
     arrayOfStrings: ['dada', 'dada', '1131'],
     email: 'kristiqn.tachev@gmail.com',
     phone: 45454222,
-    user: {
-      name: 'Трайканов',
-      arrayOfStrings: ['dada', 'dada', 'dada', 'dada', 'dada', 'dada', 'dada'],
-      email: 'kristiqn.tachev@gmail.com',
-      phone: 45454222,
-    },
   };
 }
